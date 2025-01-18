9a67cd4cd0bd575966548e92ffaee443899d2dd8
QaAutomationEngineer <damagehcmf@gmail.com>
2025-01-18 17:22:59 +0100

    Adding comments to BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -13,26 +13,35 @@ import static io.restassured.RestAssured.given;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
-
 public class BaseTest {
     protected WebDriver driver;
     private static final ConfigurationReader configuration = ConfigurationReader.getInstance();
     private static final Logger log = (Logger) LoggerFactory.getLogger(BaseTest.class);
+
+    // Indicates if the global setup has been completed; shared across all instances
     private static volatile boolean globalSetupDone = false;
+
+    // Lock object for ensuring thread-safe global setup
     private static final Object SETUP_LOCK = new Object();
 
     @BeforeAll
     public static void globalSetup() {
+        // It uses SETUP_LOCK as the lock object to prevent concurrent execution. It
+        // uses SETUP_LOCK as the lock object to prevent concurrent execution.
         synchronized (SETUP_LOCK) {
+            // Checks if the global setup has already been completed. If not, it proceeds
+            // with the setup tasks.
             if (!globalSetupDone) {
+                // Adds a key-value pair to the Mapped Diagnostic Context (MDC), which is used
+                // for logging. It helps identify logs related to the global setup.
                 MDC.put("testName", "GlobalSetup");
                 log.info("Loading test configuration");
                 log.info("Cleaning database");
                 given()
-                    .when()
-                    .get("http://localhost:3000/api/restoreDB")
-                    .then()
-                    .statusCode(201);
+                        .when()
+                        .get("http://localhost:3000/api/restoreDB")
+                        .then()
+                        .statusCode(201);
                 LoggerManager.printGlobalLogs();
                 globalSetupDone = true;
             }
@@ -41,6 +50,8 @@ public class BaseTest {
 
     @BeforeEach
     public void setup(TestInfo testInfo) {
+        // Adds a key-value pair to the Mapped Diagnostic Context (MDC), which is used
+        // for logging. It helps identify logs related to the current test. 
         MDC.put("testName", testInfo.getDisplayName());
         log.info("Starting test: {}", testInfo.getDisplayName());
         log.debug("Starting browser setup");
